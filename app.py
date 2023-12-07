28d1b78add13402b863bf96a667e2b702b9aed05
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-12-07 08:21:08 +0530

    -m

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
+
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
     """Respond to incoming phone calls with a 'Hello world' message"""
@@ -12,8 +13,7 @@ def voice():
     # Read a message aloud to the caller
     resp.say("Hello world!")
 
-    # Return a plain text response
-    return "Hello world!"
+    return str(resp)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
